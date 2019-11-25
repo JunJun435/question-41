aae573793f2bb37fcf86143c30fc53e5e43f0fae
JunJun435 <50116354+JunJun435@users.noreply.github.com>
2019-11-25 11:01:14 -0500

    Add files via upload

---
+++ origin.js
@@ -0,0 +1,8 @@
+const readlineSync = require(readline-sync);
+
+const origin = readlineSync.question("\nEnter an origin unit: ");
+const destination = readlineSync.question("Enter a destination unit: ");
+const value = readlineSync.question("enter a value: ");
+
+let oneMile;
+let
